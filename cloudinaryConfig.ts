bc08db90f51c6145fb686f2cfabd6f93cb008312
LordPrinz <plazinski2902@gmail.com>
2022-10-24 10:28:28 +0200

    Create cloudinary config

---
+++ cloudinaryConfig.ts
@@ -0,0 +1,15 @@
+import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const cloudinaryConfig = cloudinary.v2.config({
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+	secure: true,
+});
+
+const uploader = cloudinary.v2.uploader;
+
+export { cloudinaryConfig, uploader };
