f9913cc097624f38dd0f031173982f198d02cda8
LordPrinz <plazinski2902@gmail.com>
2022-10-24 10:06:22 +0200

    Connect app with error handlers

--- app.ts
+++ app.ts
@@ -5,8 +5,9 @@ import morgan from "morgan"
 import rateLimit from "express-rate-limit";
 import mongoSanitize from "express-mongo-sanitize";
 import compression from "compression";
-
+import globalErrorHandler from "./controllers/errorController"
 import { Response, Request, NextFunction, json } from "express";
+import AppError from "./utils/AppError";
 
 
 const app = express()
@@ -35,5 +36,18 @@ app.use(mongoSanitize());
 app.use(compression());
 
 app.use(json())
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        data: `Server working ${Date.now().toString()}`
+    })
+    next();
+});
+
+app.all("*", (req, res, next) => {
+    new AppError(`Can't find ${req.originalUrl} on this server!`, 404);
+})
+
+app.use(globalErrorHandler)
 
 export default app;
