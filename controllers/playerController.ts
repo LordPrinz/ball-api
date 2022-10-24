dee4e401ae9bfeba85860365a4d843f9e03e6f86
LordPrinz <plazinski2902@gmail.com>
2022-10-24 10:31:29 +0200

    Setup multer

--- controllers/playerController.ts
+++ controllers/playerController.ts
@@ -1,5 +1,31 @@
 import Player from "../models/playerModel";
 import { createOne, deleteOne, getAll, getOne, patchOne } from "./handleFactory";
+import multer from "multer";
+import AppError from "../utils/AppError";
+import { Request } from "express";
+
+const multerStorage = multer.diskStorage({
+	filename(req: Request, file, callback) {
+		const ext = file.mimetype.split("/")[1];
+		const filename = file.originalname;
+		callback(null, `player-${req.body.name}-${req.body.surname}-${Date.now()}.${ext}`);
+	},
+});
+
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: (req, file, cb) => {
+		if (file.mimetype.startsWith("image")) {
+			cb(null, true);
+		} else {
+			cb(
+				new AppError("Not an image! Please upload only images", 400) as any,
+				false
+			);
+		}
+	},
+});
+
 
 export const getAllPlayers = getAll.bind(null, Player);
 export const createPlayer = createOne.bind(null, Player);
