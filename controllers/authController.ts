a4c55334f814f8a0fb1a487fc746df3e1a91c369
LordPrinz <plazinski2902@gmail.com>
2022-10-25 22:26:59 +0200

    Add JWT

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,13 +1,30 @@
 import { NextFunction, Response, Request } from "express";
 import catchAsync from "../utils/catchAsync";
 import User from "./../models/userModel";
+import jwt from "jsonwebtoken";
 
 const signup = catchAsync(
 	async (req: Request, res: Response, next: NextFunction) => {
-		const newUser = await User.create(req.body);
+		const newUser = await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+			passwordConfirm: req.body.passwordConfirm,
+		});
+
+		const token = jwt.sign(
+			{
+				id: newUser._id,
+			},
+			process.env.JWT_SECRET!,
+			{
+				expiresIn: process.env.JWT_EXPIRES_IN!,
+			}
+		);
 
 		res.status(201).json({
 			status: "success",
+			token,
 			data: {
 				user: newUser,
 			},
