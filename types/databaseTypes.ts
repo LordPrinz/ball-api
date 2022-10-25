96eed69b639463a97032a864e345c6b4f861c8c8
LordPrinz <plazinski2902@gmail.com>
2022-10-25 18:29:51 +0200

    Add databse models types

---
+++ types/databaseTypes.ts
@@ -0,0 +1,34 @@
+import { ObjectId } from "mongoose";
+
+export enum Roles {
+	Goalkeeper,
+	"Centre-Backs",
+	"Full-Backs",
+	"Wing-Backs",
+	"Defensive Midfielders",
+	"Central Midfielders",
+	"Attacking Midfielders",
+	Wingers,
+	"Inside Forwards",
+	"Strikers",
+}
+
+export interface User {
+	_id: ObjectId;
+	name: string;
+	email: string;
+	image: string;
+	password: string;
+	__v: string;
+}
+
+export interface Player {
+	_id: ObjectId;
+	name: string;
+	surname: string;
+	age: number;
+	role: Roles;
+	club: string;
+	image: string;
+	__v: string;
+}
