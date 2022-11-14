17088b92a991503d2f179c851ab0c72f01b72d6e
LordPrinz <plazinski2902@gmail.com>
2022-11-14 09:19:29 +0100

    Implement team router

--- app.ts
+++ app.ts
@@ -11,6 +11,7 @@ import hpp from "hpp";
 import globalErrorHandler from "./controllers/errorController";
 import AppError from "./utils/AppError";
 import playerRoute from "./routes/playerRouter";
+import teamRoute from "./routes/teamRouter";
 
 const app = express();
 
@@ -56,6 +57,7 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use("/api/v1/players", playerRoute);
+app.use("/api/v1/teams", teamRoute)
 
 app.all("*", (req, res, next) => {
 	new AppError(`Can't find ${req.originalUrl} on this server!`, 404);
